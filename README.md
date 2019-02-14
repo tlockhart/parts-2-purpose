3b22a1d7679cd689312e68b1ca4c5097667e51b7
tlockhart <tony.lockhart@ymail.com>
2019-02-14 15:30:51 -0500

    updated readme

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
 
 ---
 ### IMPORTANT LINKS
-* [Link to github repository](https://github.com/NathanKloer/SciFly.git)
+* [Link to github repository](https://github.com/tlockhart/parts-2-purpose)
 
-* [Link to Production Site](https://parts-to-purpose.herokuapp.com/)
+* [Link to Production Site](https://shielded-refuge-22847.herokuapp.com/)
 
 ---
 ### **About the site**
@@ -45,3 +45,47 @@ Our goal is to provide an IT solution that would make the process easier for sma
 - Javascript/JQuery
 - ESLint
 - Heroku
+
+---
+### **Setup**
+
+In order to run the app, you will need to clone this repository and install all required technologies listed below.
+
+### **Required Technologies**
+
+1. [Node.js LTS](https://nodejs.org/en/)<br/>
+2. [NPM](https://www.npmjs.com/get-npm)<br/>
+3. [Git & Git Bash](https://git-scm.com/downloads)<br/>
+4. [MongoDB](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/)<br/>
+5. [Robo 3T](https://robomongo.org/download)<br/>
+
+### **Installation Steps**
+
+1. Open Bash and Clone the Parts2Purpose repo
+2. Install all required technologies
+3. [Optional] In Bash, change the current directory to the Parts2Purpose directory and install create-react-app module using the following command:
+    1. npm install -g create-react-app
+4. [Optional: Windows Users Only]  Run the following commands in powershell as an ADMINISTRATOR in the Parts2Purpose root directory.
+    1. npm install node-pre-gyp -g
+    2. [Close your IDE if open]
+    3. npm install --global --production windows-build-tools
+    4. npm install bcrypt
+5. In Bash, change to the Parts2Purpose directory, install all modules from the package.json, using the following command:
+    1. npm install [Enter]
+6. Seed the database
+    1. In Bash, execute the following commands from the Parts2Purpose root folder
+    2. cd scripts
+    3. node seedDb.js
+
+### **Execute Program**
+
+1. In Bash, enter the following command in the Parts2Purpose directory, to start the server.
+    1. npm start [ENTER]
+2. The application will open your browser and run the app in test mode:
+    1. http://localhost:3000
+
+### **Use**
+This repo is available for public non-commercial use only.
+
+### **Goal**
+The goal of this project is to create and deploy a MERN application, consisting of a server and a Single Page Application (SPA), using React. The server stores and retrieves data from our API using MongoDB and displays content to the client, via the UI. The UI is broken into React components, that manage and transfer state, and responds to user events.
---
+++ scripts/seedDB.js
@@ -0,0 +1,201 @@
+// Instruction: Open bash and run the following command
+// from the apps scripts directory
+// command: node seedDB.js
+
+const mongoose = require("mongoose");
+const db = require("../models");
+const prototype = (process.env.MONGODB_URI ||"mongodb://localhost/scifly");
+
+//CONNECTION STRING:
+/*******************/
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/scifly", {useNewUrlParser: true},
+  handleConnect
+);
+
+
+//Product Data:
+/*****************/
+let productIdArr = [];
+const productSeed = [
+  {
+    productName: "petri dishes",
+    description: "100 x 15 mm Round",
+    stockQuantity: 1000,
+    uom: "Unit",
+    category: "Culturing",
+    organization: "Georgia BioEd",
+    date: new Date(Date.now())
+  },
+  {
+    productName: "conical tubes",
+    description: "50 mL",
+    stockQuantity: 50,
+    uom: "Bag",
+    category: "Culturing",
+    organization: "Georgia BioEd",
+    date: new Date(Date.now())
+  },
+  {
+    productName: "microcentrifuge tubes (0.2 mL, 1.5 mL)",
+    description: "1.5mL",
+    stockQuantity: 500,
+    uom: "Unit",
+    category: "Storage",
+    organization: "Georgia BioEd",
+    date: new Date(Date.now())
+  },
+  {
+    productName: "sterile serological pipettes",
+    description: "10 uL",
+    stockQuantity: 200,
+    uom: "Unit",
+    category: "Pipettes",
+    organization: "Georgia BioEd",
+    date: new Date(Date.now())
+  }
+];
+
+//User Data
+/*******************/
+let userIdArr = [];
+const userSeed = [
+  {
+    userName: "tMiller",
+    email: "Tobby.Miller@ymail.com",
+    password: "9834u-q24j0i",
+    firstName: "Tobby",
+    lastName: "Miller",
+    school: "GT",
+    district: "ATL",
+    course: "Biology",
+    date: new Date(Date.now())
+  },
+  {
+    userName: "Marvin",
+    email: "Juel.Marvin@ymail.com",
+    password: "908q34u-[poi240",
+    firstName: "Juel",
+    lastName: "Marvin",
+    school: "GT",
+    district: "ATL",
+    course: "AP Biology",
+    date: new Date(Date.now())
+  }
+];
+
+//STEP 1: LOAD PRODUCT DATA:
+/***************************/
+function handleConnect(){
+  let db = mongoose.connection;
+  /// mongoose.connection.prototype.dropCollection(products);
+  db.dropCollection("orders", function(){
+    db.dropCollection("users", function(){
+      db.dropCollection("products", function(){
+        main();
+      });
+    });
+  });
+}
+
+
+function main(){
+  try{
+    db.Product.insertMany(productSeed, function(error, products){
+      console.log("PRODUCTLENGTH= " + products.length);
+      for(var i = 0; i < products.length; i++)
+      {
+        productIdArr[i] = products[i]._id;
+        console.log("ProductID-" + i + " " + productIdArr[i]);
+      }
+      if(i >= products.length-1){
+        createUser();
+      }
+    });
+  }
+  catch(e){
+    console.log("error = "+e);
+    process.exit(1);
+  }
+
+  //STEP 2: LOAD USER DATA:
+  /***************************/
+  createUser = () => {
+    try{
+      db.User.insertMany(userSeed, function(error, users){
+        console.log("USERLENGTH= " + users);
+        for(var i = 0; i < users.length; i++)
+        {
+          userIdArr[i] = users[i]._id;
+          console.log("USERID-" + i + " " + userIdArr[i]);
+          if(i === users.length-1){
+            createOrder();
+          }
+        }
+
+      });
+    }
+    catch(e){
+      console.log("error = "+e);
+      process.exit(1);
+    }
+  }
+
+  let orderId;
+
+  //STEP 3: LOAD PRODUCT DATA:
+  /***************************/
+  createOrder = () => {
+    db.Order.create({"ordered": true})
+    .then(function(newOrder){
+      orderId = newOrder._id;
+      console.log("NEW ORDER ID = "+newOrder._id)
+      console.log("IN THENABALE");
+
+      //IMPORTANT: CREATE AN ARRAY OF YOUR OBJECT IDS
+        db.Order.update(
+        {
+          "_id": newOrder._id
+        },
+        {
+          $push: {
+            user:  userIdArr[0],
+            products: [
+              {
+                productQuantity: 1,
+                product: productIdArr[0]._id
+              }
+            ]
+          },//define user
+        },
+        populateOrder
+      );//update query
+      return newOrder._id;
+      // populateOrder();
+    });//thenable
+  }//function
+
+  //Step4: Populate the data
+  populateOrder = () => {
+    db.Order.find({"_id": orderId})
+          // Specify that we want to populate the retrieved
+          // orders with any associated users and products
+          .populate("user")
+          .populate("products.product")
+          .then(function(data){
+              if(data.length > 0){
+                  console.log(JSON.stringify(data));
+              }
+              else{
+                  console.log("No Notes Found");
+              }
+          })
+          .catch(function(error){
+            console.log(error);
+          });
+  }
+}
