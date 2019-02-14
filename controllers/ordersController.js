e06976908483cc40148f6d31d9824df00ac39989
tlockhart <tony.lockhart@ymail.com>
2019-02-14 06:51:45 -0500

    formatted code

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -49,15 +49,10 @@ module.exports = {
         user: 'parts2pieces.info@gmail.com',
         pass: process.env.gmailPassword
       }
-      // service: 'yahoo',
-      // auth: {
-      //   user: 'parts2purpose.yahoo.com',
-      //   pass: process.env.yahooPassword
-      // }
     });
 
     const mailOptions = {
-      from: 'parts2purpose.yahoo.com',
+      from: 'parts2pieces.info@gmail.com',
       to: `${data[0].user.email}`,
       subject: `Your Recent Order ID#: ${data[0]._id}`,
       text:`Thank you, ${data[0].user.firstName}, for your order request!\n\nYour Order ID is: ${data[0]._id} and has been submitted.\n\nSomeone from the organization will be in touch with you to schedule an appointment.\n\nThank you for using Parts-to-Purpose, and we hope that you will be able to put these parts to good purpose!`
