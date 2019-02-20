da6c57c225426e8c50ff3754585eb9d57cb220a7
tlockhart <tony.lockhart@ymail.com>
2019-02-20 13:24:41 -0500

    removed page refresh in navbar on signout

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -293,7 +293,7 @@ class NavBar extends Component {
     this.setState({_id: null})
     this.props.idChanged("", "");
     document.cookie = `_uid=''; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
-    window.location.reload();
+    // window.location.reload();
     this.hideCart();
   }
   handleInputChange = event => {
